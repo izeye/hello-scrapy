afcdaed765271f0971b047654387e626dbf17518
Johnny Lim <izeye@naver.com>
2023-05-22 11:33:48 +0900

    Add a sample from a script

---
+++ myspider_from_script.py
@@ -0,0 +1,24 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+
+class MySpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['https://www.zyte.com/blog/']
+
+    def parse(self, response):
+        for title in response.css('.oxy-post-title'):
+            yield {'title': title.css('::text').get()}
+
+        for next_page in response.css('a.next'):
+            yield response.follow(next_page, self.parse)
+
+
+process = CrawlerProcess(settings={
+    "FEEDS": {
+        "items.json": {"format": "json"}
+    }
+})
+
+process.crawl(MySpider)
+process.start()
